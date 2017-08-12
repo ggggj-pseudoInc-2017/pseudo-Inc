1a3ad58ccf441ed9460e7aceac94b5718ca78137
DESKTOP-GM4CT7E\quo10 <quo1031@gmail.com>
2017-08-12 09:05:44 +0900

    Player, NPC움직임 구현

---
+++ Assets/Scripts/NpcController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcController : MonoBehaviour {
+
+    public Transform[] points;
+
+    int destPoint = 0;
+    NavMeshAgent ai;
+
+	void Start ()
+    {
+        ai = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+    {
+		if(!ai.pathPending && ai.remainingDistance < 1f)
+        {
+            GoNextPoint();
+        }
+	}
+
+    void GoNextPoint()
+    {
+        if(points.Length == 0)
+        {
+            return;
+        }
+
+        ai.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour {
+
+    NavMeshAgent ai;
+
+    public GameObject player;
+
+    bool isMovingToNpc;
+
+    void Start ()
+    {
+        ai = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+    {
+        Move();
+	}
+
+    void Move()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                if(hit.transform.tag == "npc" && (hit.transform.position - this.transform.position).magnitude > 2f)
+                {
+                    ai.destination = hit.point;
+                    isMovingToNpc = true;
+                }
+                else if(hit.transform.tag == "npc" && (hit.transform.position - this.transform.position).magnitude <= 2f)
+                {
+                    Debug.Log("npc");
+                    isMovingToNpc = false;
+                }
+                else
+                {
+                    ai.destination = hit.point;
+                    isMovingToNpc = false;
+                }
+            }
+        }
+        if (isMovingToNpc && (ai.destination - transform.position).magnitude <= 2f)
+        {
+            ai.destination = transform.position;
+        }
+        if(ai.velocity.magnitude > 0 )
+        {
+            player.GetComponent<Animator>().SetBool("isMoving", true);
+        }
+        else
+        {
+            player.GetComponent<Animator>().SetBool("isMoving", false);
+        }
+    }
+}
