71b63a70274f94fc97c11eadfb69461a7e9a6085
Probifrost <sos990721@naver.com>
2017-08-13 00:53:05 +0900

    교주방 커밋

---
+++ Assets/Scripts/potController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potController : MonoBehaviour {
+
+
+    public GameObject potPanel;
+    // Update is called once per frame
+    void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //RaycastHit hit;
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits;
+
+            RaycastHit hit;
+            Physics.Raycast(r, out hit);
+
+            if(hit.transform != null && hit.transform.tag == "pot")
+            {
+                potPanel.SetActive(true);
+            }
+
+            Debug.DrawRay(r.origin, r.direction, Color.red, 10.0f);
+
+            hits = Physics.RaycastAll(r, 1000);
+            /*
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition), 10.0f);
+                if(hit.transform.tag == "roomobj" && (hit.transform.position - this.transform.position).magnitude > 2f)
+                {
+                    crabPanel.SetActive(true);
+                }
+            } */
+            foreach(RaycastHit h in hits)
+            {
+                if (h.transform.tag == "roomobj" && (h.transform.position - this.transform.position).magnitude > 2f)
+                {
+                    potPanel.SetActive(true);
+                }
+            }
+        }
+    }
+}
